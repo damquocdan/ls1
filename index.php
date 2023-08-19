5b04f4460d5ca14e078fbfd2e73c9e403a2ebfcb
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2023-08-19 19:44:19 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+    $ten = "Dam Quoc Dan";
+    echo 'Dan dep trai';
+    
+    echo"\n xin chao toi la ".$ten;
+    $so_nguyen = 42;
+    $chuoi="hello world!";
+    $mang = array(1,2,3);
+    $sothuc = 3.14;
+    $age = 20;
+    if($age >= 18){
+        echo " Bạn đủ tuổi";
+    }else{
+        echo "bạn chưa đủ tuổi";
+    }
+?>
